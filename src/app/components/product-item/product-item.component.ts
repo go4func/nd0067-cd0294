a663a732c2a4ebb7d37c7becdd347379748fb06c
Leo <o_long_the_hai.n@kbtgvn.tech>
2024-04-21 20:06:10 +0700

    add: product list page

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../models/product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-item.component.html',
-  styleUrl: './product-item.component.css'
+  styleUrl: './product-item.component.css',
 })
 export class ProductItemComponent {
+  @Input() product: Product;
 
+  constructor() {
+    this.product = {
+      id: 0,
+      name: '',
+      price: 0,
+      url: '',
+      description: '',
+    };
+  }
 }
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Product } from '../../models/product';
+import { HttpService } from '../../services/http.service';
+import { ProductItemComponent } from '../product-item/product-item.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [ProductItemComponent, CommonModule],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
+  products: Product[] = [];
 
+  constructor(private httpSvc: HttpService) {}
+
+  ngOnInit(): void {
+    this.httpSvc.getProducts().subscribe((products) => {
+      this.products = products;
+    });
+    debugger;
+  }
 }
